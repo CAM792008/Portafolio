893107fa47b43251c55a7b735b5629af432b125c
CAM792008 <cam792008@hotmail.com>
2023-01-11 15:42:04 -0500

    Se agrego un alerta para cuando el usuario presiona los botonesencriptar o desencriptar sin haber escrito un texto.

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function encriptar() {
-	var texto = document.getElementById("inpuText").value.toLowerCase();
-	var textCifrado = texto.replace(/e/igm,"enter");
+	var texto = document.getElementById("inpuText").value.toLowerCase();//toLowerCase, para que de forma transparente convierta todas
+	var textCifrado = texto.replace(/e/igm,"enter");                   // las letras mayúsculas en minúsculas
 	var textCifrado = textCifrado.replace(/o/igm,"ober");
 	var textCifrado = textCifrado.replace(/i/igm,"imes");
 	var textCifrado = textCifrado.replace(/a/igm,"ai");
 	var textCifrado = textCifrado.replace(/u/igm,"ufat");
+	if(texto == "" || texto == +texto){
+		alert("Escriba un texto");
+		window.location.reload();
+	}
+	if(texto == "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"){
+		alert("no vale");
+		window.location.reload();
+	}
 	//los slash son para que busque todas las letras que hayan en la frase u oración
 	//la letra i; es para que haga el cambio tanto en letras mayúsculas como minúsculas
 	//la letra g; es para que busque en toda la frase y reemplace las letras
@@ -22,6 +30,10 @@ function desencriptar() {
 	var textCifrado = textCifrado.replace(/imes/igm,"i");
 	var textCifrado = textCifrado.replace(/ai/igm,"a");
 	var textCifrado = textCifrado.replace(/ufat/igm,"u");
+	if(texto == "" || texto == +texto){
+		alert("No hay nada para desencriptar");
+		window.location.reload();
+	}
 	//Esto hace que cuando el usuario de clic en encriptar, la imágen del muñeco y los textos que hay en el
 	//desaparezcan y aparezca el texto que usuario escribio ya reemplazado y tambien el cuadro de copiar
 	document.getElementById("imagen1").style.display = "none";
@@ -29,6 +41,7 @@ function desencriptar() {
 	document.getElementById("texto1").innerHTML = textCifrado;
 	document.getElementById("copiar").style.display = "show";
 	document.getElementById("copiar").style.display = "inherit";
+
 }
 function copiar() {
 	var frases = document.querySelector("#texto1");
